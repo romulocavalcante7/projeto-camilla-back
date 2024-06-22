cf1674e13a86aa4c992a17ef8a637bba0b9e1195
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-06-22 10:31:15 -0300

    feat: ajust prisma client

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-import config from './config/config';
+import { PrismaClient } from "@prisma/client";
+import config from "./config/config";
 
 // add prisma to the NodeJS global type
 interface CustomNodeJsGlobal extends Global {
@@ -9,8 +9,8 @@ interface CustomNodeJsGlobal extends Global {
 // Prevent multiple instances of Prisma Client in development
 declare const global: CustomNodeJsGlobal;
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient({ log: ["query"] });
 
-if (config.env === 'development') global.prisma = prisma;
+if (config.env === "development") global.prisma = prisma;
 
 export default prisma;
