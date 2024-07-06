2e5395e709b0594b910b04b63656ed537254fda0
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-07-06 08:13:30 -0300

    chore: config deploy teste

--- src/routes/v1/docs.route.ts
+++ src/routes/v1/docs.route.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import swaggerDefinition from '../../docs/swaggerDef';
+import express from "express";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import swaggerDefinition from "../../docs/swaggerDef";
 
 const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.ts']
+  apis: ["src/docs/*.yml", "src/routes/v1/*.ts"],
 });
 
-router.use('/', swaggerUi.serve);
+router.use("/", swaggerUi.serve);
 router.get(
-  '/',
+  "/",
   swaggerUi.setup(specs, {
-    explorer: true
+    explorer: true,
   })
 );
 
