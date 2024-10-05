f998d31e536778887c60803c6098ac68d50d2386
Wallace-Bezerra <wallace_2014_@hotmail.com>
2024-10-05 14:54:47 -0300

    add websocket

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,34 @@ import app from "./app";
 import prisma from "./client";
 import config from "./config/config";
 import logger from "./config/logger";
+import { Server as SocketIOServer } from "socket.io";
 
 let server: Server;
+let activeUsers = 0;
+
 prisma.$connect().then(() => {
   logger.info("Connected to SQL Database");
+
   server = app.listen(config.port, () => {
     logger.info(`Listening to port ${config.port}`);
+  });
+
+  const io = new SocketIOServer(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    activeUsers++;
+    io.emit("activeUsers", activeUsers);
+    logger.info(`User connected: ${socket.id}. Active users: ${activeUsers}`);
+
+    socket.on("disconnect", () => {
+      activeUsers--;
+      io.emit("activeUsers", activeUsers);
+      logger.info(`User disconnected: ${socket.id}. Active users: ${activeUsers}`);
+    });
   });
 });
 
